8385a4fe062ce3181dc283425ca6e156aa93abca
Dhanesh Purohit <dhanesh.purohit@gmail.com>
2014-05-30 00:35:46 +0530

    First commit. No error checking, just a list of commands found around the internet

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a shell script created to make the setup of a MEAN stack easy on Ubuntu servers.
+
+MEAN stands for MongoDB, Express.js, Angular.js and Node.js software stack used to develop web applications.
+
+MEAN is similar to LAMP software stack which is an abbreviation for Linux, Apache, MySQL and PHP
---
+++ mean.sh
@@ -0,0 +1,22 @@
+echo 'Configuring system for MongoDB'
+echo 'Adding MongoDB public key'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'Adding MongoDB repository to the source list'
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+echo 'Updating local package information'
+sudo apt-get update
+echo 'Installing MongoDB'
+sudo apt-get install mongodb-org -y
+echo 'Configuring system for Node.js'
+sudo apt-get install python-software-properties -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+echo 'Installing nodejs'
+sudo apt-get install python-software-properties python g++ make nodejs -y
+sudo apt-get install npm -y
+sudo apt-get install nodejs-legacy -y
+echo 'Installing Express.js'
+npm install -g express-generator
+echo 'Configuring system for Angular.js'
+npm install -g bower
